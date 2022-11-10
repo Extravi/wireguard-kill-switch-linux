19ea5ce846683f22ffe5de4e32d831712eb2affa
Extravi <98912029+Extravi@users.noreply.github.com>
2022-11-10 15:34:38 -0500

    Update vpnconnect.sh

--- vpnconnect.sh
+++ vpnconnect.sh
@@ -2,5 +2,5 @@ sudo wg-quick up wg-tunnel-name
 echo "y" | sudo ufw reset
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
-sudo ufw allow out on heckeruwu from any to any
+sudo ufw allow out on wg-tunnel-name from any to any
 sudo ufw enable
